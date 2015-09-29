9e5851220f7253813e9a2b036b5407f80c5c5481
Byron Mabbett <mabznz@gmail.com>
2015-09-29 19:42:20 +0000

    Lots of changes

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,9 @@ $(document).ready(function() {
             color: '#0baa47',
             textColor: 'black',
             subscribeText: 'Subscribe All NZ',
-            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_full_calendar.ics'
+            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_full_calendar.ics',
+            iconUnselected: 'icon sprt_ico-sport-shearing-b',
+            iconSelected: 'icon sprt_ico-sport-shearing-a'
          },
          nz_greyhound : {
             googleCalendarId: 'humaisju2feldkadb3e5b2vmguqp42pc@import.calendar.google.com',
@@ -29,25 +31,43 @@ $(document).ready(function() {
             color: '#00aaa0',
             textColor: 'black',
             subscribeText: 'Subscribe NZ Greyhound',
-            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_Greyhounds_Racing.ics'
+            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_Greyhounds_Racing.ics',
+            iconUnselected: 'icon sprt_ico-sport-greyhound-b',
+            iconSelected: 'icon sprt_ico-sport-greyhound-a'
          },
          nz_thoroughbred : {
             googleCalendarId: 'v8u0dsct3dke1uq7mpv372hg2111n4ct@import.calendar.google.com',
             className: 'thoroughbred',
             color: '#4495d1',
             textColor: 'black',
-            subscribeText: 'Subscribe Thoroughbred',
-            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_Thoroughbred_Racing.ics'
+            subscribeText: 'Subscribe NZ Thoroughbred',
+            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_Thoroughbred_Racing.ics',
+            iconUnselected: 'icon sprt_ico-sport-gallops-b',
+            iconSelected: 'icon sprt_ico-sport-gallops-a'
          },
          nz_harness : {
             googleCalendarId: '0sbabnm0m1kkia4t5hrassuj3nkr6tje@import.calendar.google.com',
             className: 'harness',
             color: '#dad9d7',
             textColor: 'black',
-            subscribeText: 'Subscribe Harness',
-            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_Harness_Racing.ics'
+            subscribeText: 'Subscribe NZ Harness',
+            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/NZ_Harness_Racing.ics',
+            iconUnselected: 'icon sprt_ico-sport-harness-b',
+            iconSelected: 'icon sprt_ico-sport-harness-a'
+         },
+         int_all : {
+            googleCalendarId: 'pl6m8itje5dbg8ahv57hib3q4nks6ef7@import.calendar.google.com',
+            className: 'int',
+            color: 'red',
+            textColor: 'black',
+            subscribeText: 'Subscribe All Harness',
+            subscribeURL: 'webcal://static.tab.co.nz/content/calendar/All_full_calendar.ics',
+            iconUnselected: '',
+            iconSelected: ''
          }
    };
+   
+   
    
    // Simple accordion with jquery ui for help section
    $(function() {
@@ -116,7 +136,7 @@ $(document).ready(function() {
    // switch calendar function, receives the id of the image the user clicked. Should match key of the calendars object 
    function switchCalendar(calType) {
       console.log('Switching to Calendar:' + calType);
-      
+      //console.log($("#country-select").val());
       // Calls the correct calendar through fullcalendar's  addEventSource call and Google Calendar API
       // Calendars are 'hosted' via a google account
       $('#calendar').fullCalendar( 'addEventSource', calendars[calType]);
@@ -129,16 +149,44 @@ $(document).ready(function() {
       }
       
       // Changes subscribe button to appropiate calendar
-      $('.subscribe_button').text(calendars[calType].subscribeText);
-      $('.subscribe_button').attr('href', calendars[calType].subscribeURL);
+      $('.btn_subscribe').text(calendars[calType].subscribeText);
+      $('.btn_subscribe').attr('href', calendars[calType].subscribeURL);
    }
    
-   // Calendar selection events
+   // Calendar Code selection events
    for (var o in calendars) {
-      $(".frame #" + o).unbind("click").click(function() {
-         if ($(this).parent().siblings(".slidee li").hasClass('active')) {
+      //get country
+      
+      
+      $("#calendar-selection #" + o).on("click", function() {
+            //alert($("#country-select").val() + "_" + $(this).attr('id'));
+            $(this).addClass( "selected" );
+            // remove selected from other items
+            for (var o in calendars) {
+               alert($(this).attr('id'));
+               if ( o != $(this).attr('id')) {
+                  $("#calendar-selection #" + o).removeClass( "selected" );
+               }
+            }
             switchCalendar($(this).attr('id'));
-         }
       });
-   } 
+   }
+   
+   // Calendar Country selection events
+   /*for (var o in calendars) {
+      $("#country-selection #" + o).change(function() {
+         switchCalendar($(this).val() + "_" + $("#country-select").val());
+      });
+   }*/
+   
+   for (var o in calendars) {
+      $("#calendar-selection #" + o).hover( function() {
+         $( this ).removeClass(calendars[$(this).attr('id')].iconUnselected);
+         $( this ).addClass(calendars[$(this).attr('id')].iconSelected);
+         
+      }, function() {
+         $( this ).removeClass(calendars[$(this).attr('id')].iconSelected);
+         $( this ).addClass(calendars[$(this).attr('id')].iconUnselected);
+      });
+   }
 });
